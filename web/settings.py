408011f6f9fea51e5209e5049d4d2404197558ef
vipulconda <73891151+vipulconda@users.noreply.github.com>
2022-07-21 02:11:59 +0530

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'newpro',
     'first',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
